34fddd3dc5d0933e0b47fd43dc5a833c98d66356
Juan Pablo Mounier <juanpablomounier@gmail.com>
2026-05-18 22:22:06 -0300

    Implement weighted text relevance scoring

--- src/text/features.py
+++ src/text/features.py
@@ -7,8 +7,30 @@ def keyword_score(text_tokens, query_tokens):
         raise ValueError("Both text_tokens and query_tokens must be non-empty lists.")
 
 
+def density_score(text_tokens, query_tokens):
+    "Calculates keywords concentration in query."
+    matching_tokens = 0
+    for token in text_tokens:
+        if token in query_tokens:
+            matching_tokens += 1
+    
+    density_score = matching_tokens / len(text_tokens)
+    return density_score
+
+def relevance_score(text_tokens, query_tokens):
+    keyword = keyword_score(text_tokens, query_tokens)
+    density = density_score(text_tokens, query_tokens)
+    relevance_score = (keyword*0.7 + density*0.3)
+    return relevance_score
+
+
+
 #TEST
-text_tokens = ["iran", "oil"]
+text_tokens = ["iran", "iran", "war", "oil"]
 query_tokens = ["iran", "war"]
 score = keyword_score(text_tokens, query_tokens)
+density = density_score(text_tokens, query_tokens)
+relevance = relevance_score(text_tokens, query_tokens)
 print(f"Keyword score: {score}")
+print(f"Density score: {density}")
+print(f"Relevance score: {relevance}")
